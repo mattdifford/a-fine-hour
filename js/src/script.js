0738c37a65d3b1c68a45fbf2ce4f82e00cab5bf0
Matt Difford <matt@mrsavvy.co.uk>
2020-02-06 15:12:20 +0000

    No longer have sign up field

--- js/src/script.js
+++ js/src/script.js
@@ -41,41 +41,39 @@ $(document).ready(function () {
             var formData = parent_form.serializeObject();
             formData.lp_campaign_id = '5e30378c0b5a5';
             formData.lp_campaign_key = 'nNXfVJ7d4Gzcwby2Zp6P';
-            if (formData.updates === "on") {
-                $.ajax({
-                    async: true,
-                    url: 'https://savvy.leadspediatrack.com/post.do',
-                    data: formData,
-                    type: 'POST',
-                    dataType: "xml",
-                    success: function (data) {
-                        var response = data.all;
-                        var nodes = [];
-                        for (i = 0; i < response.length; i++) {
-                            nodes[data.all[i].nodeName] = i;
-                        }
-                        var result_index = nodes["result"];
-                        var error_index = nodes["error"];
-                        var success = true;
-                        var msg = 'You have successfully subscribed. Check your inbox for your £15 off code';
-                        if (response[result_index].innerHTML === 'failed') {
-                            var success = false;
-                            if (response[error_index].innerHTML === 'Invalid Email') {
-                                msg = 'Please check your email address and try again';
-                            } else {
-                                msg = 'This email address is already subscribed';
-                            }
-                        }
-                        if (success) {
-                            gtag('event', 'Successful subscription', { 'event_category': 'Subscription', 'event_label': 'Subscribed from signup.afinehour.com' });
-                        }
-                        parent_form.append('<p class="form__message ' + (success ? 'form__message--success' : 'form__message--error') + '">' + msg + '</p>');
-                    },
-                    error: function (e) {
-                        parent_form.append('<p class="form__message form__message--error">Something went wrong, please try again.</p>');
+            $.ajax({
+                async: true,
+                url: 'https://savvy.leadspediatrack.com/post.do',
+                data: formData,
+                type: 'POST',
+                dataType: "xml",
+                success: function (data) {
+                    var response = data.all;
+                    var nodes = [];
+                    for (i = 0; i < response.length; i++) {
+                        nodes[data.all[i].nodeName] = i;
                     }
-                });
-            }
+                    var result_index = nodes["result"];
+                    var error_index = nodes["error"];
+                    var success = true;
+                    var msg = 'You have successfully subscribed. Check your inbox for your £15 off code';
+                    if (response[result_index].innerHTML === 'failed') {
+                        var success = false;
+                        if (response[error_index].innerHTML === 'Invalid Email') {
+                            msg = 'Please check your email address and try again';
+                        } else {
+                            msg = 'This email address is already subscribed';
+                        }
+                    }
+                    if (success) {
+                        gtag('event', 'Successful subscription', { 'event_category': 'Subscription', 'event_label': 'Subscribed from signup.afinehour.com' });
+                    }
+                    parent_form.append('<p class="form__message ' + (success ? 'form__message--success' : 'form__message--error') + '">' + msg + '</p>');
+                },
+                error: function (e) {
+                    parent_form.append('<p class="form__message form__message--error">Something went wrong, please try again.</p>');
+                }
+            });
         });
     });
 });
